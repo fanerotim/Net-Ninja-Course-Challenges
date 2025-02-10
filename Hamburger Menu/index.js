5f9e920518c8fef78de6613725913bca769fb556
Petar Cholakov <petar.cholakov30@gmail.com>
2025-02-10 07:40:51 +0000

    add animation on hamburger menu button, so when it is open button changes to X

--- Hamburger Menu/index.js
+++ Hamburger Menu/index.js
@@ -4,6 +4,9 @@ const navButton = document.querySelector('.menu-button');
 //get navigation container 
 const navContainer = document.querySelector('.navigation-container');
 
+//get line of button
+const lineElement = document.querySelector('.line');
+console.log(lineElement);
 // add event listener to nav button to toggle open / close state of menu
 navButton.addEventListener('click', () => {
     
@@ -11,11 +14,17 @@ navButton.addEventListener('click', () => {
     
     if (!isOpen) {
         navContainer.classList.add('open');
+        lineElement.classList.add('open')
         // disable page scroll when menu is open
         document.body.style.overflow = 'hidden'
+        // navButton.textContent = 'x'
     } else {
         navContainer.classList.remove('open');
+        lineElement.classList.remove('open')
         // enable page scroll when menu is closed
         document.body.style.overflow = 'auto'
+
+        // navButton.textContent = '≡'
+
     }
 })
