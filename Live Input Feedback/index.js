10636afc4f206c628b59afdbde0e2f7717bf5bca
Petar Cholakov <petar.cholakov30@gmail.com>
2025-02-13 06:35:39 +0000

    animate tick and input border on valid email

--- Live Input Feedback/index.js
+++ Live Input Feedback/index.js
@@ -1,5 +1,15 @@
 const inputElement = document.getElementById('email');
 
+const tickElement = document.querySelector('.tick');
+
 inputElement.addEventListener('keyup', (e) => {
-    console.log(inputElement.value);
+    const inputValue = inputElement.value;
+
+    if (inputValue === 'petar@abv.bg') {
+        tickElement.classList.add('valid');
+        inputElement.classList.add('valid-email');
+    } else {
+        tickElement.classList.remove('valid');
+        inputElement.classList.remove('valid-email');
+    }
 })
