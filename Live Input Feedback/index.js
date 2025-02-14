94b964e7e2f03ebf087344bc8e272780b7a87ea5
Petar Cholakov <petar.cholakov30@gmail.com>
2025-02-14 19:56:57 +0000

    add regex and check for valid email

--- Live Input Feedback/index.js
+++ Live Input Feedback/index.js
@@ -2,10 +2,14 @@ const inputElement = document.getElementById('email');
 
 const tickElement = document.querySelector('.tick');
 
+const emailRegEx = /^\w{1,}[.|_]?\w{1,}@[gmail|yahoo|abv]+\.(bg|com|net|org|dev)+$/
+
 inputElement.addEventListener('keyup', (e) => {
     const inputValue = inputElement.value;
 
-    if (inputValue === 'petar@abv.bg') {
+    const hasMatched = inputValue.match(emailRegEx);
+
+    if (hasMatched) {
         tickElement.classList.add('valid');
         inputElement.classList.add('valid-email');
     } else {
