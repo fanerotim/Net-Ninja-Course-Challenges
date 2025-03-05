8db897df6b739e949d5092cc499e2bbfca228995
Petar Cholakov <petar.cholakov30@gmail.com>
2025-03-05 05:59:34 +0000

    add solution to problem

--- Lorem Ipsum Flavour Function/index.js
+++ Lorem Ipsum Flavour Function/index.js
@@ -12,12 +12,36 @@ const flavours = {
 
 // functions
 const flavourize = (inputText, flavour) => {
-  console.log('input text: ', inputText.toUpperCase());
-  console.log('flavour: ', flavour.toUpperCase());
+
+  // get the length of current flavour, so we can then use math.Random to take randon word from flavour array
+  const flavourLength = flavours[flavour].length;
+
+  // convert inputText into array, so we can replace every third word with a random word from the flavours array
+  const inputTextAsArr = inputText.split(' ');
+
+  inputTextAsArr.forEach((text, index) => {
+
+    // create a custom index as I want to start from 1 and not 0;
+    const currentIndex = index + 1;
+
+    // get every third word, so we can replace it with a random word from flavours
+    if (currentIndex % 3 === 0) {
+
+      // generate an index randomly, which we will use to get a random word from flavours array
+      const randomIndex = Math.floor(Math.random() * flavourLength);
+      // replace every third word with a random word from the current flavour
+      inputTextAsArr[index] = flavours[flavour][randomIndex];
+    }
+  })
+
+  // get final text so we can output it on the page
+  const flavouredText = inputTextAsArr.join(' ');
+  // pass it to the updateOutput function
+  updateOutput(flavouredText);
 }
 
 const updateOutput = (text) => {
-
+  output.textContent = text;
 }
 
 // event listener
@@ -32,7 +56,7 @@ buttons.forEach(b => {
 
     // get input from user (text)
     const userInput = textarea.value;
-    
+
     flavourize(userInput, flavour);
   })
 })
