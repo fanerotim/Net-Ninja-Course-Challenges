ed3f248f6f40427dfd6791fdaeeb89d659c201f9
Petar Cholakov <petar.cholakov30@gmail.com>
2025-03-17 06:33:00 +0000

    add solution to challenge 8

---
+++ Flash card/index.js
@@ -0,0 +1,8 @@
+const card = document.querySelector('.card');
+const front = document.querySelector('.front');
+const back = document.querySelector('.back');
+
+card.addEventListener('click', (e) => {
+    
+    console.log('op!');
+})
